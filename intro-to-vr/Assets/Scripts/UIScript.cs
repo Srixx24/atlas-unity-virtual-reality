b87f66cc3a817ede71e95fd5cb858eb484d6e1ff
unknown <jacolynlovins@gmail.com>
2024-09-18 20:22:05 -0500

    Edits

--- intro-to-vr/Assets/Scripts/UIScript.cs
+++ intro-to-vr/Assets/Scripts/UIScript.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button playButton;
+    public Button exitButton;
+
+    private void Start()
     {
-        
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+        exitButton.onClick.AddListener(OnExitButtonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPlayButtonClicked()
     {
-        
+
+    }
+
+
+
+    private void OnExitButtonClicked()
+    {
+        // Quit the application
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
